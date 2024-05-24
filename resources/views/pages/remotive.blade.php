d690a6497f221f40167a0c5d84ec1e9dd500288e
michaelandrew123 <michaelandrewsuarez0@gmail.com>
2024-05-25 01:02:39 +0800

    first commit 1

--- resources/views/pages/remotive.blade.php
+++ resources/views/pages/remotive.blade.php
@@ -19,12 +19,6 @@
                         <a href="#">
                             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $job['title'] }}</h5>
                         </a>
-                        <div class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        <!-- {{ html_entity_decode(Str::limit($job['description'], 100)) }} -->
-                        
-                        <!-- {!! $job['description'] !!} -->
-                            <!-- {!! Str::limit($job['description'], 100) !!} -->
-                        </div>
                         <div class="">
                             <a href="{{ $job['url']}} " class="break inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 Read more
